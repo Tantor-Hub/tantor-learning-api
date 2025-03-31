a7af16c283d3b3654abce7a05a82cec2c7e1ecec
davmaene <davidmened@gmail.com>
2025-03-31 15:49:57 +0200

    up --version done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,50 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './configs/config';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import config from './configs/config';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: config.APP_BD_DIALECT as any,
-      host: config.APP_BD_HOST,
-      port: config.APP_BD_PORT,
-      username: config.APP_BD_USERNAME,
-      password: config.APP_BD_PASSWORD,
-      database: config.APP_BD_NAME,
-      synchronize: true, 
+    ConfigModule.forRoot({
+      isGlobal: true
     }),
-    ConfigModule.forRoot()
+    SequelizeModule.forRootAsync({
+      useFactory: async () => {
+        try {
+          return {
+            dialect: config.APP_BD_DIALECT as any,
+            host: config.APP_BD_HOST,
+            port: config.APP_BD_PORT,
+            username: config.APP_BD_USERNAME,
+            password: config.APP_BD_PASSWORD,
+            database: config.APP_BD_NAME,
+            autoLoadModels: true,
+            synchronize: true,
+            logging: console.log,
+            timezone: "+02:00",
+            retry: {
+                match: [/Deadlock/i],
+                max: 3,
+                backoffBase: 1000,
+                backoffExponent: 1.5
+            },
+            pool: {
+                max: 50,
+                min: 0,
+                acquire: 1000000,
+                idle: 200000
+            },
+          };
+        } catch (error: any) {
+          return error
+        }
+      }
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,3 +1,4 @@
+import { log } from "console";
 import { DatabaseConfig } from "src/app.interfaces";
 
 const config: DatabaseConfig = {
@@ -5,8 +6,8 @@ const config: DatabaseConfig = {
     APP_BD_HOST: process.env.APP_BD_HOST || 'localhost',
     APP_BD_PORT: Number(process.env.APP_BD_PORT) || 5432,
     APP_BD_USERNAME: process.env.APP_BD_USERNAME || 'postgres',
-    APP_BD_PASSWORD: process.env.APP_BD_PASSWORD || 'password',
-    APP_BD_NAME: process.env.APP_BD_NAME || 'mydatabase',
+    APP_BD_PASSWORD: process.env.APP_BD_PASSWORD || 'admin',
+    APP_BD_NAME: process.env.APP_BD_NAME || 'default_database',
 };
-  
+
 export default config;
