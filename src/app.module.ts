1eafe10427bcae6e62f4593b7f3839f8e31b36c3
davmaene <davidmened@gmail.com>
2025-03-31 16:00:08 +0200

    up version done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,46 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import config from './configs/config';
-import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
+      load: [databaseConfig]
     }),
     SequelizeModule.forRootAsync({
-      useFactory: async () => {
-        try {
-          return {
-            dialect: config.APP_BD_DIALECT as any,
-            host: config.APP_BD_HOST,
-            port: config.APP_BD_PORT,
-            username: config.APP_BD_USERNAME,
-            password: config.APP_BD_PASSWORD,
-            database: config.APP_BD_NAME,
-            autoLoadModels: true,
-            synchronize: true,
-            logging: console.log,
-            timezone: "+02:00",
-            retry: {
-                match: [/Deadlock/i],
-                max: 3,
-                backoffBase: 1000,
-                backoffExponent: 1.5
-            },
-            pool: {
-                max: 50,
-                min: 0,
-                acquire: 1000000,
-                idle: 200000
-            },
-          };
-        } catch (error: any) {
-          return error
-        }
-      }
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: configService.get<string>('database.dialect') as 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.database'),
+        autoLoadModels: true,
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
