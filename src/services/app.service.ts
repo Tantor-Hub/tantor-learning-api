16fca535699b8fe89f5b0c6c009d6950926d5f77
davmaene <davidmened@gmail.com>
2025-03-31 21:58:54 +0200

    up --version done

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -3,9 +3,11 @@ import { ResponseServer } from '../interface/interface.response';
 import { HttpStatusCode } from '../config/statuscodes.config';
 import { Responder } from 'src/helpers/helpers.all';
 
+const {TANTORPORT, APPNAME, APPOWNER} = process.env;
+
 @Injectable()
 export class AppService {
   getFallBackEndPoint(): ResponseServer {
-    return Responder({status: HttpStatusCode.Ok, data: null})
+    return Responder({status: HttpStatusCode.Ok, data: {appOwner: APPOWNER, appName: APPNAME}})
   }
 }
