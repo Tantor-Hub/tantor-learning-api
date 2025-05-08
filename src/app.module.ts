b7657197b86ab13fd64baafa01be39b941af114e
davmaene <davidmened@gmail.com>
2025-05-08 21:26:31 +0200

    auto loadmodel disable

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,7 +47,7 @@ import { JwtService } from './services/service.jwt';
         // port: configService.get<number>('database.port'),
         // password: configService.get<string>('database.password'),
         // database: configService.get<string>('database.database'),
-        autoLoadModels: true,
+        autoLoadModels: false,
         synchronize: true,
         dialectOptions: {
           require: true,
